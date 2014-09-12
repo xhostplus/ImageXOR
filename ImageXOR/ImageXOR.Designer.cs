49af17c4f675b735b9de374e4f8f75c3b98046e2
Bill Nelson <wnelson@bitwiddle.com>
2014-09-12 10:15:36 -0700

    Initial Commit
    Demonstrates the vulnerability of the Two Time Pad using images.

---
+++ ImageXOR/ImageXOR.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace ImageXOR
+{
+    partial class ImageXOR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImageXOR));
+            this.pictureKey = new System.Windows.Forms.PictureBox();
+            this.btnGenerateKey = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnLoadMessage1 = new System.Windows.Forms.Button();
+            this.pictureM1 = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnLoadMessage2 = new System.Windows.Forms.Button();
+            this.pictureM2 = new System.Windows.Forms.PictureBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.pictureC1 = new System.Windows.Forms.PictureBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.pictureC2 = new System.Windows.Forms.PictureBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.pictureDifference = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureKey)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureM1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureM2)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureC1)).BeginInit();
+            this.groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureC2)).BeginInit();
+            this.groupBox6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureDifference)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureKey
+            // 
+            this.pictureKey.Location = new System.Drawing.Point(26, 100);
+            this.pictureKey.Name = "pictureKey";
+            this.pictureKey.Size = new System.Drawing.Size(128, 128);
+            this.pictureKey.TabIndex = 0;
+            this.pictureKey.TabStop = false;
+            // 
+            // btnGenerateKey
+            // 
+            this.btnGenerateKey.Location = new System.Drawing.Point(40, 40);
+            this.btnGenerateKey.Name = "btnGenerateKey";
+            this.btnGenerateKey.Size = new System.Drawing.Size(100, 45);
+            this.btnGenerateKey.TabIndex = 1;
+            this.btnGenerateKey.Text = "Generate Random Key";
+            this.btnGenerateKey.UseVisualStyleBackColor = true;
+            this.btnGenerateKey.Click += new System.EventHandler(this.btnGenerateKey_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnGenerateKey);
+            this.groupBox1.Controls.Add(this.pictureKey);
+            this.groupBox1.Location = new System.Drawing.Point(20, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(180, 250);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Step 1 - Generate a Random Key";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnLoadMessage1);
+            this.groupBox2.Controls.Add(this.pictureM1);
+            this.groupBox2.Location = new System.Drawing.Point(220, 20);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(180, 250);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Step 2 - Load Image Message 1";
+            // 
+            // btnLoadMessage1
+            // 
+            this.btnLoadMessage1.Location = new System.Drawing.Point(40, 40);
+            this.btnLoadMessage1.Name = "btnLoadMessage1";
+            this.btnLoadMessage1.Size = new System.Drawing.Size(100, 45);
+            this.btnLoadMessage1.TabIndex = 1;
+            this.btnLoadMessage1.Text = "Load Image Message 1";
+            this.btnLoadMessage1.UseVisualStyleBackColor = true;
+            this.btnLoadMessage1.Click += new System.EventHandler(this.btnLoadMessage1_Click);
+            // 
+            // pictureM1
+            // 
+            this.pictureM1.Location = new System.Drawing.Point(26, 100);
+            this.pictureM1.Name = "pictureM1";
+            this.pictureM1.Size = new System.Drawing.Size(128, 128);
+            this.pictureM1.TabIndex = 0;
+            this.pictureM1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnLoadMessage2);
+            this.groupBox3.Controls.Add(this.pictureM2);
+            this.groupBox3.Location = new System.Drawing.Point(420, 20);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(180, 250);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Step 3 - Load Image Message 2";
+            // 
+            // btnLoadMessage2
+            // 
+            this.btnLoadMessage2.Location = new System.Drawing.Point(40, 40);
+            this.btnLoadMessage2.Name = "btnLoadMessage2";
+            this.btnLoadMessage2.Size = new System.Drawing.Size(100, 45);
+            this.btnLoadMessage2.TabIndex = 1;
+            this.btnLoadMessage2.Text = "Load Image Message 2";
+            this.btnLoadMessage2.UseVisualStyleBackColor = true;
+            this.btnLoadMessage2.Click += new System.EventHandler(this.btnLoadMessage2_Click);
+            // 
+            // pictureM2
+            // 
+            this.pictureM2.Location = new System.Drawing.Point(26, 100);
+            this.pictureM2.Name = "pictureM2";
+            this.pictureM2.Size = new System.Drawing.Size(128, 128);
+            this.pictureM2.TabIndex = 0;
+            this.pictureM2.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.pictureC1);
+            this.groupBox4.Location = new System.Drawing.Point(20, 290);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(180, 200);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "C1 - Message 1 XOR Key";
+            // 
+            // pictureC1
+            // 
+            this.pictureC1.Location = new System.Drawing.Point(26, 40);
+            this.pictureC1.Name = "pictureC1";
+            this.pictureC1.Size = new System.Drawing.Size(128, 128);
+            this.pictureC1.TabIndex = 0;
+            this.pictureC1.TabStop = false;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.pictureC2);
+            this.groupBox5.Location = new System.Drawing.Point(220, 290);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(180, 200);
+            this.groupBox5.TabIndex = 5;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "C2 - Message 2 XOR Key";
+            // 
+            // pictureC2
+            // 
+            this.pictureC2.Location = new System.Drawing.Point(26, 40);
+            this.pictureC2.Name = "pictureC2";
+            this.pictureC2.Size = new System.Drawing.Size(128, 128);
+            this.pictureC2.TabIndex = 0;
+            this.pictureC2.TabStop = false;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.pictureDifference);
+            this.groupBox6.Location = new System.Drawing.Point(420, 290);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(180, 200);
+            this.groupBox6.TabIndex = 6;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "C1 XOR C2";
+            // 
+            // pictureDifference
+            // 
+            this.pictureDifference.Location = new System.Drawing.Point(26, 40);
+            this.pictureDifference.Name = "pictureDifference";
+            this.pictureDifference.Size = new System.Drawing.Size(128, 128);
+            this.pictureDifference.TabIndex = 0;
+            this.pictureDifference.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 496);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(513, 155);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // ImageXOR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 670);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ImageXOR";
+            this.Text = "Two Time Pad Demonstration with Images";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureKey)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureM1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureM2)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureC1)).EndInit();
+            this.groupBox5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureC2)).EndInit();
+            this.groupBox6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureDifference)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureKey;
+        private System.Windows.Forms.Button btnGenerateKey;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnLoadMessage1;
+        private System.Windows.Forms.PictureBox pictureM1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnLoadMessage2;
+        private System.Windows.Forms.PictureBox pictureM2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.PictureBox pictureC1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.PictureBox pictureC2;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.PictureBox pictureDifference;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ImageXOR/ImageXOR.cs
@@ -0,0 +1,177 @@
+﻿/*
+ImageXOR - Two Time Pad Demonstration with Images
+Copyright (C) <2014>  <Bitwiddle, LLC>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageXOR
+{
+    public partial class ImageXOR : Form
+    {
+        private Bitmap randomKey = null;
+        private Bitmap message1 = null;
+        private Bitmap message2 = null;
+        private Bitmap ciphertext1 = null;
+        private Bitmap ciphertext2 = null;
+        private Bitmap difference = null;
+
+        private OpenFileDialog openFileDialog = null;
+
+        public ImageXOR()
+        {
+            InitializeComponent();
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpeg, *.png, *.jpg, *.gif)|*.jpeg; *.png; *.jpg; *.gif";
+        }
+
+
+        // Updates the result images if possible
+        private void UpdateUI()
+        {
+            if (randomKey != null)
+            {
+                if (message1 != null)
+                {
+                    ciphertext1 = new Bitmap(128, 128);
+
+                    // Iterate through the key and message 1 to create the ciphertext
+                    int i, j;
+                    for (i = 0; i < 128; i++)
+                    {
+                        for (j = 0; j <  128; j++)
+                        {
+                            Color m1 = message1.GetPixel(i, j);
+                            Color k1 = randomKey.GetPixel(i, j);
+
+                            int R = m1.R ^ k1.R;
+                            int G = m1.G ^ k1.G;
+                            int B = m1.B ^ k1.B;
+
+                            ciphertext1.SetPixel(i, j, Color.FromArgb(R, G, B));
+                        }
+                    }
+
+                    pictureC1.Image = ciphertext1;
+                }
+
+                if (message2 != null)
+                {
+                    ciphertext2 = new Bitmap(128, 128);
+
+                    // Iterate through the key and message 2 to create the ciphertext
+                    int i, j;
+                    for (i = 0; i < 128; i++)
+                    {
+                        for (j = 0; j < 128; j++)
+                        {
+                            Color m2 = message2.GetPixel(i, j);
+                            Color k1 = randomKey.GetPixel(i, j);
+
+                            int R = m2.R ^ k1.R;
+                            int G = m2.G ^ k1.G;
+                            int B = m2.B ^ k1.B;
+
+                            ciphertext2.SetPixel(i, j, Color.FromArgb(R, G, B));
+                        }
+                    }
+
+                    pictureC2.Image = ciphertext2;
+                }
+            }
+
+            if (message1 != null && message2 != null)
+            {
+                difference = new Bitmap(128, 128);
+
+                // Compute the difference
+                int i, j;
+                for (i = 0; i < 128; i++)
+                {
+                    for (j = 0; j < 128; j++)
+                    {
+                        Color c1 = ciphertext1.GetPixel(i, j);
+                        Color c2 = ciphertext2.GetPixel(i, j);
+
+                        int R = c1.R ^ c2.R;
+                        int G = c1.G ^ c2.G;
+                        int B = c1.B ^ c2.B;
+
+                        difference.SetPixel(i, j, Color.FromArgb(R, G, B));
+                    }
+                }
+
+                pictureDifference.Image = difference;
+            }
+
+        }
+
+        private void btnGenerateKey_Click(object sender, EventArgs e)
+        {
+            randomKey = new Bitmap(128, 128);
+            
+            // Create the image
+            int i, j;
+            Random r = new Random(System.DateTime.Now.Second);
+
+            for (i = 0; i < 128; i++)
+            {
+                for (j = 0; j < 128; j++)
+                {
+                    randomKey.SetPixel(i, j, Color.FromArgb(r.Next()));
+                }
+            }
+
+            pictureKey.Image = randomKey;
+
+            UpdateUI();
+        }
+
+        private void btnLoadMessage1_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Title = "Select the first image to be Message 1";
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // Load this image
+                message1 = new Bitmap(openFileDialog.FileName);
+                pictureM1.Image = message1;
+
+                UpdateUI();
+            }
+        }
+
+        private void btnLoadMessage2_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Title = "Select the first image to be Message 1";
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // Load this image
+                message2 = new Bitmap(openFileDialog.FileName);
+                pictureM2.Image = message2;
+
+                UpdateUI();
+            }
+        }
+    }
+}
